b15cd3dce7d06ec8ec68a2473a5481bc22e8edba
Wladimir Sidorenko <wlsidorenko@gmail.com>
2013-08-21 12:10:00 +0200

    misspelling_restorer updated

--- scripts/lib/python/ld/stringtools.py
+++ scripts/lib/python/ld/stringtools.py
@@ -12,6 +12,7 @@ from datetime import datetime
 NWORD = 0
 LOWER = 1
 UPPER = 2
+TITLE = 3
 XML_TAG = re.compile(r"(?:\s*<[^<>]+>)+$")
 XML_TAG_NAME  = re.compile(r"(?:\s*<\s*/?\s*([^\s/>]*))")
 XML_TAG_ATTRS = re.compile(r"""([^=\s]+)=(["'])(.*?)\2""")
@@ -25,32 +26,24 @@ def adjust_case(str1, str2):
     In case when str1 is longer than str2 the remaining characters
     in str1 will get the case of the last character in str2."""
     ostring = ''
-    case1 = case2 = case_diff = 0
-    for (i, (c1, c2)) in enumerate(zip(str1, str2)):
-        case1 = check_case(c1)
-        case2 = check_case(c2)
-        if case1:
-            case_diff = case2 and (case1 != case2)
-            # if cases aren't equal and both characters are letters, swap case
-            # of the character from the first string
-            if case_diff:
-                c1 = c1.swapcase()
-        ostring += c1
-    # append the rest of the 1-st string to output and adjust case if
-    # necessary
-    i += 1                  # after i-th character comes (i+1)-th boundary
-    if case_diff:
-        ostring += str1[i:].swapcase()
+    str2_case = check_case(str2)
+    if str2_case == LOWER:
+        return str1.lower()
+    elif str2_case == UPPER:
+        return str1.upper()
+    elif str2_case == TITLE and len(str1) > 1:
+        return str1[0].upper() + str1[1:].lower()
     else:
-        ostring += str1[i:]
-    return ostring
+        return str1
 
-def check_case(char):
+def check_case(istring):
     """Return case of input character."""
-    if char.islower():
+    if istring.islower():
         return LOWER
-    elif char.isupper():
+    elif istring.isupper():
         return UPPER
+    elif istring.istitle():
+        return TITLE
     else:
         return NWORD
 
