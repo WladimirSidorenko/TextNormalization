2f869441ecd22a3893a89c348ac81eadac0c3854
Wladimir Sidorenko <wlsidorenko@gmail.com>
2014-10-14 18:58:15 +0200

    OptParser updated

--- src/utils/OptParser.cpp
+++ src/utils/OptParser.cpp
@@ -14,10 +14,13 @@
 #include <stdexcept>		// std::invalid_argument
 #include <cstdlib>		// atol(), atof() etc.
 #include <cstring>		// memchr()
+#include <ostream>		// std::endl
 
 /////////////////
 // Definitions //
 /////////////////
+
+// Option
 OptParser::Option::Option(const char a_short, const char *a_long, arg_type_t a_type, \
 			  const void *a_default):
   m_short_name{a_short}, m_long_name{a_long}, m_type{a_type}, m_value{}, m_specified{false}
@@ -109,8 +112,28 @@ void OptParser::Option::parse_arg(const char *a_arg)
   }
 }
 
+const char* OptParser::Option::arg_type2str(const arg_type_t a_type)
+{
+  switch (a_type) {
+  case ArgType::CHAR_PTR:
+    return "STR";
+  case ArgType::INT:
+    return "INT";
+  case ArgType::FLOAT:
+    return "FLOAT";
+  case ArgType::DOUBLE:
+    return "DOUBLE";
+  case ArgType::LONG:
+    return "LONG";
+  case ArgType::LLONG:
+    return "LONGLONG";
+  default:
+    return "";
+  }
+}
+
 OptParser::OptParser(const char *a_desc):
-  m_short2opt{}, m_long2opt{}, m_desc{a_desc}, m_name{nullptr}, m_parsed{0}
+  m_short2opt{}, m_long2opt{}, m_desc{a_desc}, m_name{nullptr}, m_usage{}
 { }
 
 void OptParser::add_option(const char a_short, const char *a_long, \
@@ -173,6 +196,27 @@ int OptParser::parse(const int a_argc, char *a_argv[])
   return i;
 }
 
+void OptParser::usage(std::ostream& a_ostream)
+  const
+{
+  // compose description message if necessary
+  if (! *m_usage.c_str()) {
+    // add program description
+    m_usage += "DESCRIPTION:\n";
+    m_usage += m_desc;
+    // add USAGE message
+    if (m_name == nullptr) {
+      m_usage += "\nUSAGE\n";
+      m_usage += m_name;
+      m_usage += " [OPTION] [FILEs]\n";
+    }
+    // add option description
+    m_usage += "OPTIONS:\n";
+    m_usage += generate_opt_description();
+  }
+  a_ostream << m_usage << std::endl;
+}
+
 int OptParser::get_argument(const char a_short, void *a_trg)
   const
 {
@@ -183,6 +227,57 @@ int OptParser::get_argument(const char *a_long, void *a_trg)
   const
 {
   return 0;
+}
+
+std::string& OptParser::generate_opt_description()
+  const
+{
+  std::string idesc;
+  opt_shptr_t iopt;
+
+  // first add options according to their short names
+  char2opt_t::const_iterator ch_cit = m_short2opt.begin(), ch_cit_e = m_short2opt.end();
+
+  for (; ch_cit != ch_cit_e; ++ch_cit) {
+    iopt = ch_cit->second;
+    idesc += iopt->m_short_name;
+
+    if (iopt->m_long_name) {
+      idesc += "|";
+      idesc += iopt->m_long_name;
+    }
+
+    if (iopt->m_type != ArgType::NONE) {
+      idesc += Option::arg_type2str(iopt->m_type);
+    }
+
+    if (iopt->m_desc) {
+      idesc += iopt->m_desc;
+    }
+    idesc += "\n";
+  }
+
+  // then add options which don't have short names
+  str2opt_t::const_iterator s_cit = m_long2opt.begin(), s_cit_e = m_long2opt.end();
+  for (; ch_cit != ch_cit_e; ++ch_cit) {
+    if (iopt->m_short_name)
+      continue;
+
+    idesc += "--";
+    idesc += iopt->m_long_name;
+
+    if (iopt->m_type != ArgType::NONE) {
+      idesc += Option::arg_type2str(iopt->m_type);
+    }
+
+    if (iopt->m_desc) {
+      idesc += "\t";
+      idesc += iopt->m_desc;
+    }
+    idesc += "\n";
+  }
+
+  return idesc;
 }
 
 int OptParser::parse_long(const char *a_opt_start, const int a_argc, char *a_argv[], int &a_cnt)
--- src/utils/OptParser.h
+++ src/utils/OptParser.h
@@ -74,11 +74,14 @@ class OptParser
   /** Struct describing single option. */
   struct Option {
     /* Data members */
+    static const char* arg_type2str(const arg_type_t a_type);
 
     /** Short name of the option */
     const char m_short_name;
     /** Long name of the option */
     const char *m_long_name;
+    /** Option description */
+    const char *m_desc;
     /** Option's type */
     arg_type_t m_type;
     /** Option's value */
@@ -89,7 +92,7 @@ class OptParser
     /* Methods */
 
     /** option's constructor */
-    Option(const char a_short, const char *a_long, arg_type_t a_type, const void *a_default);
+    explicit Option(const char a_short, const char *a_long, arg_type_t a_type, const void *a_default);
 
     /** obtain option's value */
     const void *get_value() const;
@@ -161,11 +164,11 @@ class OptParser
   //////////////////
 
   /** Program description */
-  std::string m_desc;
+  const char *m_desc;
   /** Program description */
-  std::string m_name;
-  /** Number of processed options */
-  int m_parsed;
+  const char *m_name;
+  /** Program usage */
+  mutable std::string m_usage;
 
   /////////////
   // Methods //
@@ -176,17 +179,7 @@ class OptParser
    *
    * @param a_desc - description of the program
    */
-  OptParser(const char *a_desc = "");
-
-  /**
-   * Parse operation.
-   *
-   * @param a_argc - number of arguments
-   * @param a_argv - array of pointers to arguments
-   *
-   * @return \c int - index of the next unprocessed options (-1 if an error occurred)
-   */
-  int parse(const int a_argc, char *a_argv[]);
+  OptParser(const char *a_desc = nullptr);
 
   /**
    * Function for adding option to parser.
@@ -199,6 +192,25 @@ class OptParser
    */
   void add_option(const char a_short, const char *a_long, const char *a_desc, \
 		  arg_type_t a_type = ArgType::NONE, void *a_default = nullptr);
+
+  /**
+   * Parse operation.
+   *
+   * @param a_argc - number of arguments
+   * @param a_argv - array of pointers to arguments
+   *
+   * @return \c int - index of the next unprocessed options (-1 if an error occurred)
+   */
+  int parse(const int a_argc, char *a_argv[]);
+
+  /**
+   * Output usage information and exit.
+   *
+   * @param a_ostream - output stream for usage
+   *
+   * @return \c void
+   */
+  void usage(std::ostream& a_ostream) const;
 
   /**
    * Obtain value of option's argument.
@@ -221,6 +233,42 @@ class OptParser
    * argument were specified
    */
   int get_argument(const char *a_long, void *a_trg) const;
+
+  /**
+   * Check if option was specified on command line.
+   *
+   * @param a_short - short name of the option
+   *
+   * @return \c true if option was specified, \c false otherwise
+   */
+  inline bool check(const char a_short)
+    const
+  {
+    char2opt_t::const_iterator cit = m_short2opt.find(a_short);
+
+    if (cit == m_short2opt.end())
+      return false;
+    else
+      return cit->second->m_specified;
+  }
+
+  /**
+   * Check if option was specified on command line.
+   *
+   * @param a_long - long name of the option
+   *
+   * @return \c true if option was specified, \c false otherwise
+   */
+  inline bool check(const char *a_long)
+    const
+  {
+    str2opt_t::const_iterator cit = m_long2opt.find(std::string(a_long));
+
+    if (cit == m_long2opt.end())
+      return false;
+    else
+      return cit->second->m_specified;
+  }
 
  private:
   /**
@@ -254,6 +302,13 @@ class OptParser
    * @return \c void
    */
   void get_arg_value(opt_shptr_t a_opt, const char *a_arg_start);
+
+  /**
+   * Generate description of an option.
+   *
+   * @return C++ string holding option description
+   */
+  std::string& generate_opt_description() const;
 };
 
 #endif /*__OPTPRASER_H__ */
